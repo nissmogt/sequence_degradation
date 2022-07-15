9d98a0361e83bd2c0d0b1825a2d6fd73f1de5e92
nissmogt <k.mehrabiani@gmail.com>
2022-07-14 17:20:15 -0700

    added dca ranking and zscore reference/function call

--- analysis_pipeline.py
+++ analysis_pipeline.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
-import sys
+import data.tools.pdb as dpdb
 import analysis.dca_object as dca
 
 # sysid = sys.argv[1].strip(".fa")
@@ -9,20 +9,21 @@ root = os.getcwd()
 nseqs = 5165
 raw_dca = f"DI_{sysid}_n{nseqs}.txt"
 dir_dca = os.path.join(root, "systems", sysid, "replicates", "sub0", "mf", "pc0.2")
+dca_in = os.path.join(dir_dca, raw_dca)
 
 d = dca.DirectCoupling()
 d._sysid = sysid
 d._score = "diapc"
 neff_array = np.load(os.path.join(root, "systems", sysid, "replicates", "neff_array.npy"))
 n_effective = neff_array[0][0]
-dca_in = os.path.join(dir_dca, raw_dca)
+out_dca = os.path.join(dir_dca, f"{sysid}_neff{n_effective}_all.txt")
 
-d.load_raw(dca_in)
-d.rank_hamming()
-df_dca = d._ranked_dataframe
-dir_dca_in = os.path.dirname(dca_in)
-outfile = os.path.join(dir_dca_in, f"{sysid}_neff{n_effective}_pc0.2_all.txt")
+df = d.load_raw(dca_in)
+df_rank = d.rank_hamming(df, distance=5)
+df_pdb = dpdb.pipeline_pdb(sysid, "pdb")
+df_dca = df_rank.merge(df_pdb, how='inner', on=['i', 'j'])
 
-zscore_reference = np.load(os.path.join(root, f"monomer_{'diapc'.upper()}_scores.npy"))
-# TODO: add reference file
-df_z = d.zscore(df_dca, zscore_reference)
+zscore_reference = np.load(os.path.join(root, "assets", f"monomer_DIAPC_scores.npy"))
+df_z = d.zscore(df_rank, zscore_reference)
+
+# d.savetocsv(df_z, out_dca)
