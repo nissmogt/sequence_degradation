892611a700e5cd8e800e370dc8b02891ba672af4
nissmogt <k.mehrabiani@gmail.com>
2022-07-31 15:29:22 -0700

    created run_analysis.py

---
+++ run_analysis.py
@@ -0,0 +1,18 @@
+import os
+import numpy as np
+from analysis.analysis_pipeline import pipeline_replicates
+from msa.tools.check_length import check_length
+
+# sysid = sys.argv[1].strip(".fa")
+sysid = "1a3aA"
+root = os.getcwd()
+msa = os.path.join("systems", sysid, f"{sysid}_filtered_25.fasta")
+seq_l = check_length(msa)
+zbool = True
+dca_mode = "mf"
+if zbool:
+    threshold_values = [12, 10, 9, 8, 5.6, 4.5, 4, 3.5, 2.5, 1]
+else:
+    threshold_values = np.arange(10, 110, 10)
+
+ppl = pipeline_replicates(sysid, seq_l, threshold_values, npairs=0, zfilter=True, plots=True, passthrough=False)
