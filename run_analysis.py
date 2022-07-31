cca134c6ad1b86560f561eea5f7a9065f42cb4ac
nissmogt <k.mehrabiani@gmail.com>
2022-07-31 16:40:46 -0700

    added dca_dir

--- run_analysis.py
+++ run_analysis.py
@@ -5,11 +5,10 @@ from analysis.analysis_pipeline import pipeline_replicates
 from msa.tools.check_length import check_length
 
 sysid = sys.argv[1].strip(".fa")
-root = os.getcwd()
-print(f"System ID: {sysid}\nRoot folder: {root}\n")
-
+print(f"System ID: {sysid}\n")
+dca_dir = os.path.join("/scratch", "kmm5")
 # sysid = "1a3aA"
-msa = os.path.join("systems", sysid, f"{sysid}_filtered_25.fasta")
+msa = os.path.join(dca_dir, "systems", sysid, f"{sysid}_filtered_25.fasta")
 seq_l = check_length(msa)
 zbool = True
 dca_mode = "mf"
@@ -18,4 +17,4 @@ if zbool:
 else:
     threshold_values = np.arange(10, 110, 10)
 
-ppl = pipeline_replicates(sysid, seq_l, threshold_values, npairs=0, zfilter=True, plots=True, passthrough=False)
+ppl = pipeline_replicates(dca_dir, sysid, seq_l, threshold_values, npairs=0, zfilter=True, plots=True, passthrough=False)
