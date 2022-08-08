6daff0d10b7521aa88034cf4c926fe8d56e91c71
nissmogt <k.mehrabiani@gmail.com>
2022-08-08 10:22:40 -0700

    removed new reference bug that changed zscore reference each time

--- analysis/zscore.py
+++ analysis/zscore.py
@@ -1,13 +1,16 @@
 import numpy as np
 
 
+def add_systems(scores, reference):
+    return np.concatenate((reference, scores))
+
+
 def zscore_calculation(dca_dataframe, dca_score, reference):
     # calc zscore
     # reference = np.load(f"dimers_{dca_score.upper()}_scores.npy")
     scores = dca_dataframe[dca_score].to_numpy()
-    new_reference = np.concatenate((reference, scores))
-    mean = np.mean(new_reference)
-    std = np.std(new_reference)
+    mean = np.mean(reference)
+    std = np.std(reference)
     z = np.zeros_like(scores)
     for k in range(len(scores)):
         z[k] = (scores[k] - mean) / std
