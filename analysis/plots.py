38760930e3e32bc62055bf948e3ca7c6161b68f2
nissmogt <k.mehrabiani@gmail.com>
2022-09-08 11:28:45 -0700

    formatting plot title

--- analysis/plots.py
+++ analysis/plots.py
@@ -17,7 +17,7 @@ def contact_map_single(couplings=None, monomer=None, n=10, x="text", distance_cu
     plt.show()
 
 
-def compare_contact_map(dca_dataframe, dca_filtered, pdb_dataframe, pdbid, distance_cutoff, average_neff, zcut,
+def compare_contact_map(dca_dataframe, dca_filtered, pdb_dataframe, pdbid, distance_cutoff, n_effective, zcut,
                         Lseq, index_shift, dir_fig, extra_text=None):
     xy_lim = np.abs(Lseq + index_shift)
     top_2n = 2 * Lseq
@@ -35,8 +35,8 @@ def compare_contact_map(dca_dataframe, dca_filtered, pdb_dataframe, pdbid, dista
     plt.ylabel("residue j")
     plt.xlim(0, Lseq + 5)
     plt.ylim(0, Lseq + 5)
-    plt.title(f"<Neff>={average_neff: .2f}, <Neff>/L={average_neff / Lseq:.2f}")
-    imgfile = os.path.join(dir_fig, f"{extra_text}z{zcut}_top{top_2n}_{average_neff}.png")
+    plt.title(f"<Neff>={n_effective:.2f}, <Neff>/L={n_effective / Lseq:.2f}")
+    imgfile = os.path.join(dir_fig, f"{extra_text}z{zcut}_top{top_2n}_{n_effective:.2f}.png")
     plt.savefig(imgfile, format="png", dpi=200, bbox_inches='tight')
     plt.close()
 
@@ -45,7 +45,7 @@ def plot_score_distribution(dca_score, n_seqs, dca_dataframe, n_effective, n_res
     plt.figure(n_seqs, figsize=(5, 5))
     heights, bins, patches = plt.hist(dca_dataframe[dca_score], bins=50)
     plt.semilogy()
-    plt.title(f"Neff={n_effective}, Neff/L={n_effective / n_res:.2f}")
+    plt.title(f"Neff={n_effective:.2f}, Neff/L={n_effective / n_res:.2f}")
     plt.xlim(-10, 21)
     plt.xticks(np.arange(-10, 24, 3))
     mean = dca_dataframe[dca_score].mean()
@@ -54,7 +54,7 @@ def plot_score_distribution(dca_score, n_seqs, dca_dataframe, n_effective, n_res
     plt.xlabel("Z-score")
     plt.ylabel("Counts")
     plt.legend(loc="upper right")
-    img_out = os.path.join(img_dir, f"{extra_text}{dca_score}_neff{n_effective}.png")
+    img_out = os.path.join(img_dir, f"{extra_text}{dca_score}_neff{n_effective:.2f}.png")
     plt.savefig(img_out, format="png", dpi=200, bbox_inches='tight')
     plt.close()
 
@@ -63,31 +63,31 @@ def plot_dist_distribution(dca_dataframe, n_effective, n_res, img_dir, extra_tex
     plt.figure(8927384929, figsize=(5, 5))
     heights, bins, patches = plt.hist(dca_dataframe["d"], bins=50, color="red", edgecolor="black")
     plt.ylim(0, 60)
-    plt.title(f"Neff={n_effective}, Neff/L={n_effective / n_res:.2f}")
+    plt.title(f"Neff={n_effective:.2f}, Neff/L={n_effective / n_res:.2f}")
     plt.xlabel("intra-chain distance $\AA$")
     plt.ylabel("counts")
     plt.legend(loc="best")
-    img_out = os.path.join(img_dir, f"{extra_text}distance_neff{n_effective}.png")
+    img_out = os.path.join(img_dir, f"{extra_text}distance_neff{n_effective:.2f}.png")
     plt.savefig(img_out, format="png", dpi=200, bbox_inches='tight')
     plt.close()
 
 
-def plot_ppv(zfilter, ppv_list, neff_value, pfamid, sequence_len, threshold_val, img_dir, extra_text=None):
+def plot_ppv(zfilter, ppv_list, n_effective, pfamid, sequence_len, threshold_val, img_dir, extra_text=None):
     plt.figure(figsize=(5, 5))
     plt.plot(ppv_list)
     plt.scatter(range(len(ppv_list)), ppv_list, edgecolors="black", label=f"z={threshold_val:.1f}")
     plt.ylim(-0.1, 1.1)
     plt.xlabel("rank")
     plt.ylabel("PPV")
-    plt.title(f"{pfamid}, Neff:{neff_value:.2f}, Neff/L={neff_value / sequence_len:.2f}")
+    plt.title(f"{pfamid}, Neff:{n_effective:.2f}, Neff/L={n_effective / sequence_len:.2f}")
     plt.grid(which="both", alpha=0.3)
     plt.legend(loc="best")
 
     if zfilter:
-        img_out = os.path.join(img_dir, f"{extra_text}ppv_neff{neff_value}_z{threshold_val:.1f}.png")
+        img_out = os.path.join(img_dir, f"{extra_text}ppv_neff{n_effective:.2f}_z{threshold_val:.1f}.png")
         plt.savefig(img_out, format="png", dpi=200, bbox_inches='tight')
     else:
-        img_out = os.path.join(img_dir, f"{extra_text}ppv_neff{neff_value}_top{threshold_val:.1f}.png")
+        img_out = os.path.join(img_dir, f"{extra_text}ppv_neff{n_effective:.2f}_top{threshold_val:.1f}.png")
         plt.savefig(img_out, format="png", dpi=200, bbox_inches='tight')
     plt.close()
 
@@ -99,20 +99,20 @@ def plot_top_zscore(dca_dataframe, n, n_effective, n_res, img_dir, extra_text=No
     plt.figure(873499, figsize=(5, 5))
     plt.plot(topn_dca)
     plt.scatter(range(n), topn_dca, edgecolors="black", label=f"avg:{avg_z:.2f}, std:{std_z:.2f}")
-    plt.title(f"Neff={n_effective}, Neff/L={n_effective / n_res:.2f}")
+    plt.title(f"Neff={n_effective:.2f}, Neff/L={n_effective / n_res:.2f}")
     plt.xlabel("rank")
     plt.ylabel("z-score")
     plt.ylim(0, 25)
     plt.legend(loc="best")
-    img_out = os.path.join(img_dir, f"{extra_text}top{n}_avgz_neff{n_effective}.png")
+    img_out = os.path.join(img_dir, f"{extra_text}top{n}_avgz_neff{n_effective:.2f}.png")
     plt.savefig(img_out, format="png", dpi=200, bbox_inches='tight')
     plt.close()
 
 
-def plot_average_ppv(ppv_array, neff_array, sysid, _sys_l, z, _dir_out, norm=1):
+def plot_average_ppv(ppv_array, n_effective_array, sysid, _sys_l, z, _dir_out, norm=1):
     plt.figure(0)
-    _r, _n = neff_array.shape
-    avg_neff_l = np.mean(neff_array, axis=0) / _sys_l
+    _r, _n = n_effective_array.shape
+    avg_neff_l = np.mean(n_effective_array, axis=0) / _sys_l
     avg_ppv = np.mean(ppv_array, axis=0)
     std_ppv = np.std(ppv_array, axis=0)
     outfile = os.path.join(_dir_out, f"avgppv_std_z{z}.png")
