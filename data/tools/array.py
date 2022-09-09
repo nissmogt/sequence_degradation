d1cade07c7a168d3a2c129d189b5cddd4aa0c0cb
nissmogt <k.mehrabiani@gmail.com>
2022-09-09 16:14:54 -0700

    calculates ratio below distance threshold

--- data/tools/array.py
+++ data/tools/array.py
@@ -3,3 +3,7 @@ import numpy as np
 
 def save_array(array, outname):
     np.save(outname, array)
+
+
+def calculate_ratio_below_threshold(array, threshold):
+    return np.count_nonzero(array <= threshold) / len(array)
