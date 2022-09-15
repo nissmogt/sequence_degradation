75c3fec4ccb1cd68907fedc6dc2d10caa82dfbd2
nissmogt <k.mehrabiani@gmail.com>
2022-09-15 15:31:55 -0700

    added check_nseq method

--- msa/tools/check_length.py
+++ msa/tools/check_length.py
@@ -3,3 +3,8 @@ def check_length(filein):
     from Bio import AlignIO
     align = AlignIO.read(open(filein), "fasta")
     return align.get_alignment_length()
+
+def check_nseq(filein):
+    from Bio import AlignIO
+    align = AlignIO.read(open(filein), "fasta")
+    return len(align)
