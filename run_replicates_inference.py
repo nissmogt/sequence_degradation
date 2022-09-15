a069f4225f35b81fa08ea680d6e6cc6c86777e5c
nissmogt <k.mehrabiani@gmail.com>
2022-09-15 12:51:24 -0700

    created separate inference for replicates

---
+++ run_replicates_inference.py
@@ -0,0 +1,20 @@
+#!/home/kmm5/.conda/envs/bio/bin/python3.8
+import os
+import sys
+import data.system_object
+
+sysid = sys.argv[1].strip(".fa")
+aln_dir = sys.argv[2]
+root = os.getcwd()
+out = "/scratch/kmm5/"
+matlab_dir = os.path.join(root, "mf_plm_reweight")
+print(f"System ID: {sysid}\nRoot folder: {root}\noutput folder: {out}")
+
+s = data.system_object.System()
+s.set_sysid(sysid)
+s.set_align_dir(os.path.join(root, aln_dir))
+s.make_new_dirs(root, out)
+s.check_dir()
+s.filter(run=True)
+list_len = s.replicates(run=True)
+list_neff = s.run_inference(list_len, 70, matlab_dir, passthrough=False)
